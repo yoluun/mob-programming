0f16219f090d63d7729b2139308ea11d24980af9
yoluun <32120252+yoluun@users.noreply.github.com>
2018-01-17 10:56:52 +0900

    Add files via upload

---
+++ facefunctions.py
@@ -0,0 +1,18 @@
+#fbapi
+from fbkey import *
+
+def detectFaces(img):
+
+	return 0
+
+def createPersonGroup(personGroupId, name, userData=None):
+	return 0
+
+def createPerson(personGroupId, name, userData=None):
+	return 0 
+
+def applyFace(personGroupId, personId, image):
+	return 0 
+
+def verifyFace(image, personGroupId=None, personId=None):
+	return 0 
